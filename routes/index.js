6d3131f364eaee8777c85d2094fb17f3cea14e80
Unix162 <artur.unix@gmail.com>
2026-07-13 09:30:14 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
-// הגדרת משתנה עם יכולות ראוטר
 const router = express.Router();
 
-// הגדרת ראוטר
-// הפרמטר הראשון הכתובת ביחס למה שיוגדר בראוט
-// הפרמטר השני פונקציה של שרת איך להגיב
+
 router.get("/",async(req,res) => {
   res.json({msg:"index work 222"})
 })
