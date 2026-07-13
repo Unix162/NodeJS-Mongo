2acf52376e4ca57eff292c7cb53b280cbb00f04b
Unix162 <artur.unix@gmail.com>
2026-07-13 09:29:49 +0300

    Update configRoutes.js

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,4 +1,3 @@
-// הגדרת ראוטים
 const indexR = require("./index");
 const usersR = require("./users");
 const toysR = require("./toys");
@@ -6,9 +5,7 @@ const toysR = require("./toys");
 
 
 
-// נייצר ונייצא פונקציה שמגדירה את כל הראוטים
 exports.configRoutes = (app) => {
-  // הגדרת ראוט כללי
   app.use("/",indexR)
   app.use("/users",usersR)
   app.use("/toys",toysR)
